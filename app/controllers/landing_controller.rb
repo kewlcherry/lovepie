01a42662eae5112efa24740184c9c5d04556d207
Acsia <acsia@acsia-laptop.(none)>
2009-09-20 03:45:18 +0100

    working on the actual payment and drill down of the value for each organisation compared to the number of tages it gets

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,9 +1,19 @@
 class LandingController < ApplicationController
-
+  before_filter :require_user, :only => :add_organisation_to_cause
+  
   def index
     @organisations = Organisation.all
     @causes = Cause.find_by_mapp
   end
   
+  def add_organisation_to_cause
+    @organisation = params[:organisation_id]
+    @cause = params[:cause_id]
+    mapping = Mapping.new
+    mapping.user=@current_user
+    mapping.organisation=@organisation
+    mapping.cause=@cause
+    mapping.save
+  end
   
 end
