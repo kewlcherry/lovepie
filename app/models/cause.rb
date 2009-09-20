7f6f089bc5d774b55cef529d130dbf62658a2f28
Acsia <acsia@acsia-laptop.(none)>
2009-09-20 01:50:58 +0100

    awesomness

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -3,7 +3,11 @@ class Cause < ActiveRecord::Base
   has_many :organisations, :through => :mappings
   has_many :users, :through => :mappings
   
-  def find_by_mapp
-    Cause.find(:all, :select => 'causes.*, count(causes.id) AS count_all', :joins => :mappings, :group => 'causes.id', :order => 'count_all DESC')
+  def self.find_by_mapp
+    Cause.find(:all, 
+      :select => 'causes.*, count(causes.id) AS count_all',
+      :joins => :mappings, 
+      :group => 'causes.id',
+      :order => 'count_all DESC')
   end
 end
