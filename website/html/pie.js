3047c3ad85cc19f903b79c7254e6117d7fe6ad8f
Kevin McDonagh <kevin@novoda.com>
2011-09-25 00:52:02 +0100

    refactored the pie so as to move the styling into the main index.

---
+++ website/html/pie.js
@@ -0,0 +1,29 @@
+function initPie(){
+	var data = [];
+	var series = Math.floor(Math.random()*10)+1;
+	for( var i = 0; i<series; i++)
+	{
+		data[i] = { label: "Series"+(i+1), data: Math.floor(Math.random()*100)+1 }
+	}
+
+	$.plot($("#heart-pie"), data, 
+	{
+		series: {
+			pie: { 
+				show: true,
+				radius:300,
+				label: {
+					show: false,
+					formatter: function(label, series){
+						return '<div style="font-size:8pt;text-align:center;padding:2px;color:white;">'+label+'<br/>'+Math.round(series.percent)+'%</div>';
+					},
+					threshold: 0.1
+				}
+			}
+		},
+		legend: {
+			show: false
+		}
+	});
+	
+}	
